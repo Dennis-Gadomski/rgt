9f71637a1c32e64dcabedb457768408434ae0afc
dennis.gadomski@gmail.com <dennis.gadomski@gmail.com>
2021-07-31 12:16:47 +0200

    remove unused import

--- src/main/java/com/gadomski/dennis/rgt/database/repository/PlayerRepository.java
+++ src/main/java/com/gadomski/dennis/rgt/database/repository/PlayerRepository.java
@@ -4,9 +4,6 @@ import com.gadomski.dennis.rgt.database.model.Player;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PlayerRepository extends JpaSpecificationExecutor<Player> {
-    List<Player> findAll();
 }
