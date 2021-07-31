0a4bf3a9c657e52e7404e8a7584c1374346697b3
dennis.gadomski@gmail.com <Dcoolg_1234>
2021-07-31 10:26:27 +0200

    init project

---
+++ src/test/java/com/gadomski/dennis/rgt/RgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadomski.dennis.rgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
