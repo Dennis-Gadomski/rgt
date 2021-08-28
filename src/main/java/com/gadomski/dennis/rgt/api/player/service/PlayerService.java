6763188dfe5b1c09999dc900ee1bce90fc42afca
dennis.gadomski@gmail.com <dennis.gadomski@gmail.com>
2021-08-28 11:02:11 +0200

    add method testing git config

--- src/main/java/com/gadomski/dennis/rgt/api/player/service/PlayerService.java
+++ src/main/java/com/gadomski/dennis/rgt/api/player/service/PlayerService.java
@@ -1,4 +1,9 @@
 package com.gadomski.dennis.rgt.api.player.service;
 
+import com.gadomski.dennis.rgt.database.model.Player;
+
+import java.util.List;
+
 public interface PlayerService {
+    List<Player> getPlayers();
 }
